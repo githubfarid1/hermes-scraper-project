5092bba2d1ce56f55d474379bb5f3e09924e63bd
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-10-18 08:33:10 +0900

    update scripts

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def get_cookies():
         urldecoy = urls[random.choice(range(0, len(urls)))]
         print("get new cookies", '...', flush=True, end="")
         proxycamoufox, proxies = get_new_proxy()
-        with Camoufox(headless="virtual", proxy=proxycamoufox, geoip=True, os=('macos'), screen=Screen(max_width=1920, max_height=1080)) as browser:
+        with Camoufox(headless=True, proxy=proxycamoufox, geoip=True, os=('macos','windows', 'linux'), screen=Screen(max_width=1920, max_height=1080)) as browser:
             page = browser.new_page(locale="en-US")
             try:
                 response = page.goto(urldecoy, timeout=60000, wait_until="networkidle")    
